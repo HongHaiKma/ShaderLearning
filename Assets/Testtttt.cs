bce00e68aead1fdd25cdbd1f42e31e8de8a4f4eb
HongHaiKma <nguyenhonghailqd1996@gmail.com>
2021-07-12 15:26:44 +0700

    no message

--- Assets/Testtttt.cs
+++ Assets/Testtttt.cs
@@ -7,12 +7,13 @@ public class Testtttt : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Transform tf = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
